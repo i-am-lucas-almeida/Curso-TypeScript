ae4aa85f0d5482e0f70eefee8d595bd95d23f107
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-06-15 16:55:59 -0300

    Adiciona aula 06.06 - Introdução ao TypeScript

---
+++ 1_INTRO/index.js
@@ -0,0 +1,12 @@
+"use strict";
+const firstName = 'Lucas';
+const anotherName = 1;
+function printName(name) {
+    console.log('Esse é o nome = ' + name);
+}
+printName(firstName);
+// Desafio
+function soma(numA, numB) {
+    console.log('A soma dos dois números é: ' + (numA + numB));
+}
+soma(5, 10);
---
+++ 1_INTRO/index.ts
@@ -0,0 +1,20 @@
+const firstName = 'Lucas';
+const anotherName = 1;
+
+function printName(name: string) {
+
+    console.log('Esse é o nome = ' + name)
+
+}
+
+printName(firstName);
+
+// Desafio
+
+function soma(numA : number, numB : number) {
+
+    console.log('A soma dos dois números é: ' + (numA + numB));
+
+}
+
+soma(5, 10);
